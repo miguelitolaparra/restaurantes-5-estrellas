9fa6b3342b96905d792cf8728023e685a002f692
miguelito <miguelestabaenlaparra@gmail.com>
2022-06-20 14:54:10 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import { NavigationContainer } from "@react-navigation/native"
+import { AppNavigation } from "./src/navigation/AppNavigation"
+import  Toast  from "react-native-toast-message"
+import { initFirebase } from "./src/utils"
+import 'react-native-get-random-values'
+
+export default function App() {
+  return (
+    <>
+      <NavigationContainer>
+        <AppNavigation/>
+      </NavigationContainer>
+
+      <Toast />
+    </>
+  )
+}
